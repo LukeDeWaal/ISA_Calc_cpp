9e223bd4d6e3c587f296663de68b06b6f634c49a
Luke de Waal <lr.de.waal.01@gmail.com>
2019-11-17 20:22:28 +0100

    Console argument options now enabled

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
+#include <string>
 #include "src/Atmosphere.h"
 
-int main() {
+int main(int argc, char* argv[]) {
+
+	double hmin, hmax, hstep;
+
+	if(argc == 4){
+		hmin = std::stod(argv[1]);
+		hmax = std::stod(argv[2]);
+		hstep = std::stod(argv[3]);
+	}
+	else{
+		hmin = 0;
+		hmax = 80000.0; 
+		hstep = 100.0;
+	}
+
     Atmosphere A = Atmosphere();
-    std::cout << A.calculate(20000)[2] << std::endl;
+    std::array<double, 3> result;
+    std::cout << "Height [m] - Temperature [K] - Pressure [Pa] - Density [kg/m^3]" << std::endl;
+
+    for(float h = hmin; h <= hmax; h+=hstep){
+        result = A.calculate(h);
+        std::cout << h << " - " << result[0] << " - " << result[1] << " - " << result[2] << "\n";
+    }
+
+    // Just to keep console window open
+    std::cout << "\nPress Enter to Exit" << std::endl;
+    std::cin.ignore();
+
+
     return 0;
 }
